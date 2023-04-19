31b930932e7233e32d7ad1a0fdabfa90fb499852
Hannes Braun <hannesbraun@mail.de>
2023-04-19 20:22:48 +0200

    Fix bug in enable/disable penalty script for new server version
    The scripts destroyed the naosoccersim.rb for the new server version,
    because we have multiple settings that start with PenaltyShootout now.
    
    Original commit by Klaus Dorer

--- scripts/simspark/simsparkEnablePenalty.sh
+++ scripts/simspark/simsparkEnablePenalty.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 RB_FILE=/usr/local/share/rcssserver3d/naosoccersim.rb
 [ -f $RB_FILE ] || RB_FILE=/usr/share/rcssserver3d/naosoccersim.rb
-sudo sed -i "s/\(addSoccerVar(.PenaltyShootout.\).*)/\1, true)/" $RB_FILE
+sudo sed -i "s/\(addSoccerVar(.PenaltyShootout'\).*)/\1, true)/" $RB_FILE
 killall -9 rcssserver3d
